88db0f8fad6d9d2f230820a9a4c17b9badc89fa7
fmglimlengco <fmglimlengco@gmail.com>
2025-02-16 13:39:01 -0500

    stage 2

---
+++ server.js
@@ -0,0 +1,69 @@
+var HTTP_PORT = process.env.PORT || 8080;
+var express = require("express");
+var app = express();
+var path = require("path");
+var collegeData = require("./modules/collegeData");
+
+app.get("/", (req, res) => {
+    res.sendFile(path.join(__dirname, "views/home.html"));
+});
+
+app.get("/about", (req, res) => {
+    res.sendFile(path.join(__dirname, "views/about.html"));
+});
+
+app.get("/htmlDemo", (req, res) => {
+    res.sendFile(path.join(__dirname, "views/htmlDemo.html"));
+});
+
+app.get("/students", (req, res) => {
+    if (req.query.course) {
+        collegeData.getStudentsByCourse(req.query.course).then((data) => {
+            res.json(data);
+        }).catch((err) => {
+            res.json({ message: err });
+        });
+    } else {
+        collegeData.getAllStudents().then((data) => {
+            res.json(data);
+        }).catch((err) => {
+            res.json({ message: err });
+        });
+    }
+});
+
+app.get("/tas", (req, res) => {
+    collegeData.getTAs().then((data) => {
+        res.json(data);
+    }).catch((err) => {
+        res.json({ message: err });
+    });
+});
+
+app.get("/courses", (req, res) => {
+    collegeData.getCourses().then((data) => {
+        res.json(data);
+    }).catch((err) => {
+        res.json({ message: err });
+    });
+});
+
+app.get("/student/:num", (req, res) => {
+    collegeData.getStudentByNum(req.params.num).then((data) => {
+        res.json(data);
+    }).catch((err) => {
+        res.json({ message: err });
+    });
+});
+
+app.use((req, res) => {
+    res.status(404).send("Page Not Found");
+});
+
+collegeData.initialize().then(() => {
+    app.listen(HTTP_PORT, () => {
+        console.log(`server listening on port: ${HTTP_PORT}`);
+    });
+}).catch((err) => {
+    console.log(`Error: ${err}`);
+});
