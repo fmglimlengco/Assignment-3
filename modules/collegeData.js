9e90b3aae006420834780b647f6eada79d23b30f
fmglimlengco <fmglimlengco@gmail.com>
2025-03-08 19:35:18 -0500

    Assignment 4

--- modules/collegeData.js
+++ modules/collegeData.js
@@ -1,7 +1,7 @@
 const fs = require("fs");
 
-class Data{
-    constructor(students, courses){
+class Data {
+    constructor(students, courses) {
         this.students = students;
         this.courses = courses;
     }
@@ -10,15 +10,17 @@ class Data{
 let dataCollection = null;
 
 module.exports.initialize = function () {
-    return new Promise( (resolve, reject) => {
-        fs.readFile('./data/courses.json','utf8', (err, courseData) => {
+    return new Promise((resolve, reject) => {
+        fs.readFile("./data/courses.json", "utf8", (err, courseData) => {
             if (err) {
-                reject("unable to load courses"); return;
+                reject("unable to load courses");
+                return;
             }
 
-            fs.readFile('./data/students.json','utf8', (err, studentData) => {
+            fs.readFile("./data/students.json", "utf8", (err, studentData) => {
                 if (err) {
-                    reject("unable to load students"); return;
+                    reject("unable to load students");
+                    return;
                 }
 
                 dataCollection = new Data(JSON.parse(studentData), JSON.parse(courseData));
@@ -26,101 +28,85 @@ module.exports.initialize = function () {
             });
         });
     });
-}
+};
 
-module.exports.getAllStudents = function(){
-    return new Promise((resolve,reject)=>{
+module.exports.getAllStudents = function () {
+    return new Promise((resolve, reject) => {
         if (dataCollection.students.length == 0) {
-            reject("query returned 0 results"); return;
+            reject("query returned 0 results");
+            return;
         }
 
         resolve(dataCollection.students);
-    })
-}
+    });
+};
 
 module.exports.getTAs = function () {
-    return new Promise(function (resolve, reject) {
-        var filteredStudents = [];
-
-        for (let i = 0; i < dataCollection.students.length; i++) {
-            if (dataCollection.students[i].TA == true) {
-                filteredStudents.push(dataCollection.students[i]);
-            }
-        }
+    return new Promise((resolve, reject) => {
+        let filteredStudents = dataCollection.students.filter(student => student.TA === true);
 
         if (filteredStudents.length == 0) {
-            reject("query returned 0 results"); return;
+            reject("query returned 0 results");
+            return;
         }
 
         resolve(filteredStudents);
     });
 };
 
-module.exports.getCourses = function(){
-   return new Promise((resolve,reject)=>{
-    if (dataCollection.courses.length == 0) {
-        reject("query returned 0 results"); return;
-    }
+module.exports.getCourses = function () {
+    return new Promise((resolve, reject) => {
+        if (dataCollection.courses.length == 0) {
+            reject("query returned 0 results");
+            return;
+        }
 
-    resolve(dataCollection.courses);
-   });
+        resolve(dataCollection.courses);
+    });
 };
 
 module.exports.getStudentByNum = function (num) {
-    return new Promise(function (resolve, reject) {
-        var foundStudent = null;
+    return new Promise((resolve, reject) => {
+        let student = dataCollection.students.find(student => student.studentNum == num);
 
-        for (let i = 0; i < dataCollection.students.length; i++) {
-            if (dataCollection.students[i].studentNum == num) {
-                foundStudent = dataCollection.students[i];
-            }
+        if (!student) {
+            reject("query returned 0 results");
+            return;
         }
 
-        if (!foundStudent) {
-            reject("query returned 0 results"); return;
-        }
-
-        resolve(foundStudent);
+        resolve(student);
     });
 };
 
 module.exports.getStudentsByCourse = function (course) {
-    return new Promise(function (resolve, reject) {
-        var filteredStudents = [];
-
-        for (let i = 0; i < dataCollection.students.length; i++) {
-            if (dataCollection.students[i].course == course) {
-                filteredStudents.push(dataCollection.students[i]);
-            }
-        }
+    return new Promise((resolve, reject) => {
+        let filteredStudents = dataCollection.students.filter(student => student.course == course);
 
         if (filteredStudents.length == 0) {
-            reject("query returned 0 results"); return;
+            reject("query returned 0 results");
+            return;
         }
 
         resolve(filteredStudents);
     });
 };
 
-module.exports.getStudentsByCourse = function(course) {
+// Step 6: Implement addStudent
+module.exports.addStudent = function (studentData) {
     return new Promise((resolve, reject) => {
-      let studentsByCourse = students.filter(student => student.course == course);
-      if (studentsByCourse.length > 0) {
-        resolve(studentsByCourse);
-      } else {
-        reject("no results returned");
-      }
+        try {
+            // Ensure TA is a boolean
+            studentData.TA = studentData.TA === "true"; // If "true", convert to boolean
+
+            // Assign a unique student number
+            studentData.studentNum = dataCollection.students.length + 1;
+
+            // Add student to the array
+            dataCollection.students.push(studentData);
+
+            resolve();
+        } catch (error) {
+            reject("Unable to add student.");
+        }
     });
-  };
-  
-  module.exports.getStudentByNum = function(num) {
-    return new Promise((resolve, reject) => {
-      let student = students.find(student => student.studentNum == num);
-      if (student) {
-        resolve(student);
-      } else {
-        reject("no results returned");
-      }
-    });
-  };
-  
+};
--- server.js
+++ server.js
@@ -1,13 +1,12 @@
 /*********************************************************************************
-*  WEB700 – Assignment 03
-*  I declare that this assignment is my own work in accordance with Seneca  Academic Policy.  No part 
-*  of this assignment has been copied manually or electronically from any other source 
+*  WEB700 – Assignment 04
+*  I declare that this assignment is my own work in accordance with Seneca Academic Policy.  
+*  No part of this assignment has been copied manually or electronically from any other source 
 *  (including 3rd party web sites) or distributed to other students.
 * 
-*  Name: Frances Limlengco Student ID: 180927238 Date: 02/19/2025
+*  Name: Frances Limlengco Student ID: 180927238 Date: 03/08/2025
 *
 ********************************************************************************/ 
-
 
 var HTTP_PORT = process.env.PORT || 8080;
 var express = require("express");
@@ -15,65 +14,80 @@ var app = express();
 var path = require("path");
 var collegeData = require("./modules/collegeData");
 
+// Middleware to serve static files (CSS, images, etc.)
+app.use(express.static("public"));
+app.use(express.urlencoded({ extended: true })); // Middleware for form data
+
+// Home Route
 app.get("/", (req, res) => {
     res.sendFile(path.join(__dirname, "views/home.html"));
 });
 
+// About Route
 app.get("/about", (req, res) => {
     res.sendFile(path.join(__dirname, "views/about.html"));
 });
 
+// HTML Demo Route
 app.get("/htmlDemo", (req, res) => {
     res.sendFile(path.join(__dirname, "views/htmlDemo.html"));
 });
 
+// Get all students or filter by course
 app.get("/students", (req, res) => {
     if (req.query.course) {
-        collegeData.getStudentsByCourse(req.query.course).then((data) => {
-            res.json(data);
-        }).catch((err) => {
-            res.json({ message: err });
-        });
+        collegeData.getStudentsByCourse(req.query.course)
+            .then((data) => res.json(data))
+            .catch((err) => res.json({ message: err }));
     } else {
-        collegeData.getAllStudents().then((data) => {
-            res.json(data);
-        }).catch((err) => {
-            res.json({ message: err });
-        });
+        collegeData.getAllStudents()
+            .then((data) => res.json(data))
+            .catch((err) => res.json({ message: err }));
     }
 });
 
+// Get all TAs
 app.get("/tas", (req, res) => {
-    collegeData.getTAs().then((data) => {
-        res.json(data);
-    }).catch((err) => {
-        res.json({ message: err });
-    });
+    collegeData.getTAs()
+        .then((data) => res.json(data))
+        .catch((err) => res.json({ message: err }));
 });
 
+// Get all courses
 app.get("/courses", (req, res) => {
-    collegeData.getCourses().then((data) => {
-        res.json(data);
-    }).catch((err) => {
-        res.json({ message: err });
-    });
+    collegeData.getCourses()
+        .then((data) => res.json(data))
+        .catch((err) => res.json({ message: err }));
 });
 
+// Get a single student by student number
 app.get("/student/:num", (req, res) => {
-    collegeData.getStudentByNum(req.params.num).then((data) => {
-        res.json(data);
-    }).catch((err) => {
-        res.json({ message: err });
-    });
+    collegeData.getStudentByNum(req.params.num)
+        .then((data) => res.json(data))
+        .catch((err) => res.json({ message: err }));
 });
 
+// GET Route: Display "Add Student" Form (Step 4)
+app.get("/students/add", (req, res) => {
+    res.sendFile(__dirname + "/views/addStudent.html");
+});
+
+// POST Route: Handle "Add Student" Form Submission (Step 5)
+app.post("/students/add", (req, res) => {
+    collegeData.addStudent(req.body)
+        .then(() => res.redirect("/students")) // Redirect to students list
+        .catch(err => res.status(500).send("Error adding student: " + err));
+});
+
+// 404 Error Page for Unmatched Routes
 app.use((req, res) => {
     res.status(404).send("Page Not Found");
 });
 
+// Start the server after initializing data
 collegeData.initialize().then(() => {
     app.listen(HTTP_PORT, () => {
-        console.log(`server listening on port: ${HTTP_PORT}`);
+        console.log(`Server listening on port: ${HTTP_PORT}`);
     });
 }).catch((err) => {
     console.log(`Error: ${err}`);
